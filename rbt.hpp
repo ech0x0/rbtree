29a6ce0703cc175297847a5f3e4ec816abe778e0
ech0x0 <141404426+ech0x0@users.noreply.github.com>
2023-10-18 19:54:58 +0300

    Added constructors, destructors, insert, delete

--- rbt.hpp
+++ rbt.hpp
@@ -4,12 +4,16 @@
 
 template<class T>
 struct Node {
-    Node* parent;
-    Node* left;
-    Node* right;
+    Node<T>* parent;
+    Node<T>* left;
+    Node<T>* right;
     //0 -> black, 1 -> red
     char color;
     T data;
+
+    Node();
+    Node(Node<T>* iParent, Node<T>* iLeft, Node<T>* iRight, char iColor, T iData);
+    ~Node();
 };
 
 template<class T>
@@ -18,6 +22,7 @@ private:
     Node<T>* root;
 public:
     RBT();
+    ~RBT();
 
     Node<T>* getRoot() const;
 
@@ -29,4 +34,104 @@ public:
     void rightRotate(Node<T>* node);
 };
 
-#endif RBT_HPP
+template<class T>
+inline Node<T>::Node() : data() {
+    parent = nullptr;
+    left = nullptr;
+    right = nullptr;
+    color = 0;
+}
+
+template<class T>
+inline Node<T>::Node(Node<T>* iParent, Node<T>* iLeft, Node<T>* iRight, char iColor, T iData) : data(iData) {
+    parent = iParent;
+    left = iLeft;
+    right = iRight;
+    color = iColor;
+}
+
+template<class T>
+inline Node<T>::~Node() {
+    if (left) delete left;
+    if (right) delete right;
+}
+
+template<class T>
+inline RBT<T>::RBT() {
+    root = new Node<T>();
+}
+
+template<class T>
+inline RBT<T>::~RBT() {
+    if (root) delete root;
+}
+
+template<class T>
+inline Node<T>* RBT<T>::getRoot() const {
+    return root;
+}
+
+template<class T>
+inline void RBT<T>::insert(T data) {
+}
+
+template<class T>
+inline void RBT<T>::remove(T data) {
+}
+
+template<class T>
+inline Node<T>* RBT<T>::search(T data) const {
+    return nullptr;
+}
+
+template<class T>
+inline void RBT<T>::leftRotate(Node<T>* node) {
+    Node<T>* x = node->right;
+    node->right = x->left;
+
+    if (x->left != nullptr) {
+        x->left->parent = node;
+    }
+
+    x->parent = node->parent;
+
+    if (node->parent == nullptr) {
+        this->root = x;
+    }
+    else if (node == node->parent->left) {
+        node->parent->left = x;
+    }
+    else {
+        node->parent->right = x;
+    }
+
+    x->left = node;
+    node->parent = x;
+}
+
+template<class T>
+inline void RBT<T>::rightRotate(Node<T>* node) {
+    Node<T>* x = node->left;
+    node->left = x->right;
+
+    if (x->right != nullptr) {
+        x->right->parent = node;
+    }
+
+    x->parent = node->parent;
+
+    if (node->parent == nullptr) {
+        this->root = x;
+    }
+    else if (node == node->parent->right) {
+        node->parent->right = x;
+    }
+    else {
+        node->parent->left = x;
+    }
+
+    x->right = node;
+    node->parent = x;
+}
+
+#endif
