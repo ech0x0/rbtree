83f417d00e6029675441d38e848b8ff7787c71a3
ech0x0 <141404426+ech0x0@users.noreply.github.com>
2023-10-22 15:01:15 +0300

    Added remove

--- rbtree.hpp
+++ rbtree.hpp
@@ -3,50 +3,53 @@
 #define RBT_HPP
 
 template<class T>
-struct Node {
-    Node<T>* parent;
-    Node<T>* left;
-    Node<T>* right;
+struct RBNode {
+    RBNode<T>* parent;
+    RBNode<T>* left;
+    RBNode<T>* right;
     //0 -> black, 1 -> red
     char color;
     T data;
 
-    Node();
-    Node(Node<T>* iParent, Node<T>* iLeft, Node<T>* iRight, char iColor, T iData);
-    ~Node();
+    RBNode();
+    RBNode(RBNode<T>* iParent, RBNode<T>* iLeft, RBNode<T>* iRight, char iColor, T iData);
+    ~RBNode();
 };
 
 template<class T>
 class RBTree {
 private:
-    Node<T>* root;
+    RBNode<T>* root;
 
-    void fixTreeAfterInsert(Node<T>* node);
+    void fixTreeAfterInsert(RBNode<T>* node);
+    void fixTreeAfterRemove(RBNode<T>* node);
+    //set node x to y in x's parent pointers
+    void updateNodeParent(RBNode<T>* x, RBNode<T>* y);
 public:
     RBTree();
     ~RBTree();
 
-    Node<T>* getRoot() const;
+    RBNode<T>* getRoot() const;
 
     void insert(T data);
-    void remove(T data);
+    void remove(RBNode<T>* node);
 
-    Node<T>* searchFromNode(Node<T>* node, T data) const;
-    Node<T>* search(T data) const;
+    RBNode<T>* searchFromNode(RBNode<T>* node, T data) const;
+    RBNode<T>* search(T data) const;
 
-    void preorder(Node<T>* node, void (*func)(Node<T>* x));
-    void inorder(Node<T>* node, void (*func)(Node<T>* x));
-    void postorder(Node<T>* node, void (*func)(Node<T>* x));
+    void preorder(RBNode<T>* node, void (*func)(RBNode<T>* x));
+    void inorder(RBNode<T>* node, void (*func)(RBNode<T>* x));
+    void postorder(RBNode<T>* node, void (*func)(RBNode<T>* x));
 
-    Node<T>* minimum(Node<T>* node) const;
-    Node<T>* maximum(Node<T>* node) const;
+    RBNode<T>* minimum(RBNode<T>* node) const;
+    RBNode<T>* maximum(RBNode<T>* node) const;
 
-    void leftRotate(Node<T>* node);
-    void rightRotate(Node<T>* node);
+    void leftRotate(RBNode<T>* node);
+    void rightRotate(RBNode<T>* node);
 };
 
 template<class T>
-inline Node<T>::Node() : data() {
+inline RBNode<T>::RBNode() : data() {
     parent = nullptr;
     left = nullptr;
     right = nullptr;
@@ -54,7 +57,7 @@ inline Node<T>::Node() : data() {
 }
 
 template<class T>
-inline Node<T>::Node(Node<T>* iParent, Node<T>* iLeft, Node<T>* iRight, char iColor, T iData) : data(iData) {
+inline RBNode<T>::RBNode(RBNode<T>* iParent, RBNode<T>* iLeft, RBNode<T>* iRight, char iColor, T iData) : data(iData) {
     parent = iParent;
     left = iLeft;
     right = iRight;
@@ -62,14 +65,14 @@ inline Node<T>::Node(Node<T>* iParent, Node<T>* iLeft, Node<T>* iRight, char iCo
 }
 
 template<class T>
-inline Node<T>::~Node() {
+inline RBNode<T>::~RBNode() {
     if (left) delete left;
     if (right) delete right;
 }
 
 template<class T>
-inline void RBTree<T>::fixTreeAfterInsert(Node<T>* node) {
-    Node<T>* x;
+inline void RBTree<T>::fixTreeAfterInsert(RBNode<T>* node) {
+    RBNode<T>* x;
 
     while (node->parent->color == 1) {
         if (node->parent == node->parent->parent->right) {
@@ -122,6 +125,87 @@ inline void RBTree<T>::fixTreeAfterInsert(Node<T>* node) {
 }
 
 template<class T>
+inline void RBTree<T>::fixTreeAfterRemove(RBNode<T>* node) {
+    RBNode<T>* x;
+    while (node != root && node->color == 0) {
+        if (node == node->parent->left) {
+            x = node->parent->right;
+            if (x != nullptr && x->color == 1) {
+                x->color = 0;
+                node->parent->color = 1;
+                leftRotate(node->parent);
+                x = node->parent->right;
+            }
+
+            if (x != nullptr && (x->left == nullptr || x->left->color == 0) && (x->right == nullptr || x->right->color == 0)) {
+                x->color = 1;
+                node = node->parent;
+            }
+            else {
+                if (x && (x->right == nullptr || x->right->color == 0)) {
+                    if (x->left) x->left->color = 0;
+                    x->color = 1;
+                    rightRotate(x);
+                    x = node->parent->right;
+                }
+
+                if (x) x->color = node->parent->color;
+                node->parent->color = 0;
+                if (x && x->right) x->right->color = 0;
+                leftRotate(node->parent);
+                node = root;
+            }
+        }
+        else {
+            x = node->parent->left;
+            if (x != nullptr && x->color == 1) {
+                x->color = 0;
+                node->parent->color = 1;
+                rightRotate(node->parent);
+                x = node->parent->left;
+            }
+
+            if (x != nullptr && (x->left == nullptr || x->left->color == 0) && (x->right == nullptr || x->right->color == 0)) {
+                x->color = 1;
+                node = node->parent;
+            }
+            else {
+                if (x != nullptr && x->left != nullptr && x->left->color == 0) {
+                    if (x->right) x->right->color = 0;
+                    x->color = 1;
+                    leftRotate(x);
+                    x = node->parent->left;
+                }
+
+                if (x) x->color = node->parent->color;
+                node->parent->color = 0;
+                if (x && x->left) x->left->color = 0;
+                rightRotate(node->parent);
+                node = root;
+            }
+        }
+    }
+    node->color = 0;
+}
+
+template<class T>
+inline void RBTree<T>::updateNodeParent(RBNode<T>* x, RBNode<T>* y) {
+    if (x == nullptr) return;
+
+    if (x->parent == nullptr) {
+        root = y;
+    }
+    else if (x == x->parent->left) {
+        x->parent->left = y;
+    }
+    else {
+        x->parent->right = y;
+    }
+
+    if (y != nullptr) y->parent = x->parent;
+}
+
+template<class T>
 inline RBTree<T>::RBTree() {
     root = nullptr;
 }
@@ -132,21 +216,21 @@ inline RBTree<T>::~RBTree() {
 }
 
 template<class T>
-inline Node<T>* RBTree<T>::getRoot() const {
+inline RBNode<T>* RBTree<T>::getRoot() const {
     return root;
 }
 
 template<class T>
 inline void RBTree<T>::insert(T data) {
-    Node<T>* node = new Node<T>();
+    RBNode<T>* node = new RBNode<T>();
     node->parent = nullptr;
     node->data = data;
     node->left = nullptr;
     node->right = nullptr;
     node->color = 1;
 
-    Node<T>* y = nullptr;
-    Node<T>* x = this->root;
+    RBNode<T>* y = nullptr;
+    RBNode<T>* x = this->root;
 
     while (x != nullptr) {
         y = x;
@@ -182,11 +266,54 @@ inline void RBTree<T>::insert(T data) {
 }
 
 template<class T>
-inline void RBTree<T>::remove(T data) {
+inline void RBTree<T>::remove(RBNode<T>* node) {
+    if (node == nullptr) return;
+
+    RBNode<T>* x;
+    char originalColor = node->color;
+
+    if (node->left == nullptr && node->right == nullptr) {
+        x = node->parent;
+        updateNodeParent(node, nullptr);
+    }
+    else if (node->left == nullptr) {
+        x = node->right;
+        updateNodeParent(node, node->right);
+    }
+    else if (node->right == nullptr) {
+        x = node->left;
+        updateNodeParent(node, node->left);
+    }
+    else {
+        RBNode<T>* y = minimum(node->right);
+        originalColor = y->color;
+        x = y->right;
+        if (y->parent == node) {
+            if (x) x->parent = y;
+        }
+        else {
+            updateNodeParent(y, y->right);
+            y->right = node->right;
+            y->right->parent = y;
+        }
+
+        updateNodeParent(node, y);
+        y->left = node->left;
+        y->left->parent = y;
+        y->color = node->color;
+    }
+
+    node->left = nullptr;
+    node->right = nullptr;
+    delete node;
+
+    if (originalColor == 0) {
+        if (x) fixTreeAfterRemove(x);
+    }
 }
 
 template<class T>
-inline Node<T>* RBTree<T>::searchFromNode(Node<T>* node, T data) const {
+inline RBNode<T>* RBTree<T>::searchFromNode(RBNode<T>* node, T data) const {
     if (node == nullptr || node->data == data) return node;
 
     if (data < node->data) return searchFromNode(node->left, data);
@@ -194,12 +321,12 @@ inline Node<T>* RBTree<T>::searchFromNode(Node<T>* node, T data) const {
 }
 
 template<class T>
-inline Node<T>* RBTree<T>::search(T data) const {
+inline RBNode<T>* RBTree<T>::search(T data) const {
     return searchFromNode(root, data);
 }
 
 template<class T>
-inline void RBTree<T>::preorder(Node<T>* node, void(*func)(Node<T>* x)) {
+inline void RBTree<T>::preorder(RBNode<T>* node, void(*func)(RBNode<T>* x)) {
     if (node != nullptr) {
         func(node);
         preorder(node->left, func);
@@ -208,7 +335,7 @@ inline void RBTree<T>::preorder(Node<T>* node, void(*func)(Node<T>* x)) {
 }
 
 template<class T>
-inline void RBTree<T>::inorder(Node<T>* node, void(*func)(Node<T>* x)) {
+inline void RBTree<T>::inorder(RBNode<T>* node, void(*func)(RBNode<T>* x)) {
     if (node != nullptr) {
         inorder(node->left, func);
         func(node);
@@ -217,7 +344,7 @@ inline void RBTree<T>::inorder(Node<T>* node, void(*func)(Node<T>* x)) {
 }
 
 template<class T>
-inline void RBTree<T>::postorder(Node<T>* node, void(*func)(Node<T>* x)) {
+inline void RBTree<T>::postorder(RBNode<T>* node, void(*func)(RBNode<T>* x)) {
     if (node != nullptr) {
         postorder(node->left, func);
         postorder(node->right, func);
@@ -226,8 +353,8 @@ inline void RBTree<T>::postorder(Node<T>* node, void(*func)(Node<T>* x)) {
 }
 
 template<class T>
-inline Node<T>* RBTree<T>::minimum(Node<T>* node) const {
-    Node<T>* x = node;
+inline RBNode<T>* RBTree<T>::minimum(RBNode<T>* node) const {
+    RBNode<T>* x = node;
     while (x->left != nullptr) {
         x = x->left;
     }
@@ -235,8 +362,8 @@ inline Node<T>* RBTree<T>::minimum(Node<T>* node) const {
 }
 
 template<class T>
-inline Node<T>* RBTree<T>::maximum(Node<T>* node) const {
-    Node<T>* x = node;
+inline RBNode<T>* RBTree<T>::maximum(RBNode<T>* node) const {
+    RBNode<T>* x = node;
     while (x->right != nullptr) {
         x = x->right;
     }
@@ -244,8 +371,8 @@ inline Node<T>* RBTree<T>::maximum(Node<T>* node) const {
 }
 
 template<class T>
-inline void RBTree<T>::leftRotate(Node<T>* node) {
-    Node<T>* x = node->right;
+inline void RBTree<T>::leftRotate(RBNode<T>* node) {
+    RBNode<T>* x = node->right;
     node->right = x->left;
 
     if (x->left != nullptr) {
@@ -269,8 +396,8 @@ inline void RBTree<T>::leftRotate(Node<T>* node) {
 }
 
 template<class T>
-inline void RBTree<T>::rightRotate(Node<T>* node) {
-    Node<T>* x = node->left;
+inline void RBTree<T>::rightRotate(RBNode<T>* node) {
+    RBNode<T>* x = node->left;
     node->left = x->right;
 
     if (x->right != nullptr) {
